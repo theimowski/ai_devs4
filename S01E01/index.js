73b7b2961dd1b7de849a2cb7394c51933773633e
Tomasz Heimowski <tomek.heimowski@gmail.com>
2026-03-09 15:30:41 +0100

    Download the CSV

--- S01E01/index.js
+++ S01E01/index.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+const fs = require('fs');
+
+if (!fs.existsSync('people.csv')) {
+  const key = process.env.HUB_AG3NTS_KEY;
+  const url = `https://hub.ag3nts.org/data/${key}/people.csv`;
+  fetch(url)
+    .then(res => res.text())
+    .then(text => fs.writeFileSync('people.csv', text));
+}
